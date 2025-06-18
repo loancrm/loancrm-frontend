152d283fc07b75760cef416d05ddbc38e0d7ec89
DandamuriSantharaju <rajusantha040@gmail.com>
2025-06-18 18:26:53 +0530

    Validations

---
+++ src/app/otp.service.ts
@@ -0,0 +1,16 @@
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+
+// @Injectable({ providedIn: 'root' })
+// export class OtpService {
+//     constructor(private http: HttpClient) {}
+
+//     sendOtp(phone: string) {
+//         return this.http.post('http://localhost:5002/otp/send-otp', { phone });
+//     }
+
+//     verifyOtp(phone: string, otp: string) {
+//         return this.http.post('http://localhost:5002/otp/verify-otp', { phone, otp });
+//     }
+// }
+
